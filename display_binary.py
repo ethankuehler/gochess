9e62763009d6a69daab0534c3f5ef34886287874
ethankuehler <ethankuehler@gmail.com>
2024-10-21 01:35:26 -0400

    fixed spelling error

--- display_binary.py
+++ display_binary.py
@@ -17,8 +17,8 @@ def alg_to_int(s:str) -> int:
 
 
 def alg_to_shift(s:str) -> int:
-    coloums = ['a','b','c','d','e','f']
-    col = coloums.index(s[0])
+    columns = ['a','b','c','d','e','f']
+    col = columns.index(s[0])
     row = int(s[1])
     return (col + (row-1)*8)
 
