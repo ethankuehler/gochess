b8ef93c4dde96363415c79560489f171ff30ac71
ethankuehler <ethankuehler@gmail.com>
2024-11-01 00:18:55 -0400

    formated python code

--- display_binary.py
+++ display_binary.py
@@ -4,19 +4,21 @@ from enum import Enum
 import pandas as pd
 import itertools
 
-#this shit is so scuffed XD
+
+# this shit is so scuffed XD
 class Colour(Enum):
     white = 0
     black = 1
 
-class PositionIter():
+
+class PositionIter:
     def __init__(self, start: str, stop: str):
-        self.columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        self.columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
         self.idx = [self.columns.index(start[0]), int(start[1])]
         self.stop = [self.columns.index(stop[0]), int(stop[1])]
 
     def _less_eq(self, rhs, lhs):
-        return (rhs[0] + rhs[1]*8) <= (lhs[0] + lhs[1]*8)
+        return (rhs[0] + rhs[1] * 8) <= (lhs[0] + lhs[1] * 8)
 
     def __iter__(self):
         return self
@@ -33,26 +35,23 @@ class PositionIter():
             self.idx[1] += 1
         return r
 
-class Position:
-    def __init__(self, pos = 'a1'):
-        self.columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        self.loc = [self.columns.index(pos[0]), int(pos[1]) - 1]
 
+class Position:
+    def __init__(self, pos="a1"):
+        self.columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        self.loc = [self.columns.index(pos[0]), int(pos[1]) - 1]
 
     def __lt__(self, lhs):
         rhs = self.loc
-        return (rhs[0] + rhs[1]*8) < (lhs[0] + lhs[1]*8)
-
+        return (rhs[0] + rhs[1] * 8) < (lhs[0] + lhs[1] * 8)
 
     def __le__(self, lhs):
         rhs = self.loc
-        return (rhs[0] + rhs[1]*8) <= (lhs[0] + lhs[1]*8)
-
+        return (rhs[0] + rhs[1] * 8) <= (lhs[0] + lhs[1] * 8)
 
     def __eq__(self, lhs):
         rhs = self.loc
         return rhs == lhs
-
 
     def add(self, col, row):
         new_col = self.loc[0] + col
@@ -64,37 +63,35 @@ class Position:
         else:
             return None
 
-
     def getInt(self):
-        return 1 << (self.loc[0] + self.loc[1]*8)
-
+        return 1 << (self.loc[0] + self.loc[1] * 8)
 
     def getShift(self):
-        return (self.loc[0] + self.loc[1]*8)
-
+        return self.loc[0] + self.loc[1] * 8
 
     def getString(self):
         return self.columns[self.loc[0]] + str(self.loc[1] + 1)
 
 
 def display_binary(x: int) -> str:
-    bstring = '{0:064b}'.format(x)
-    s = [bstring[i:i+8][::-1] for i in range(0, len(bstring), 8)]
-    return '\n'.join(s)
+    bstring = "{0:064b}".format(x)
+    s = [bstring[i : i + 8][::-1] for i in range(0, len(bstring), 8)]
+    return "\n".join(s)
 
 
-def alg_to_int(s:str) -> int:
-    columns = ['a','b','c','d','e','f', 'g', 'h']
+def alg_to_int(s: str) -> int:
+    columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
     col = columns.index(s[0])
     row = int(s[1])
-    return 1 << (col + (row-1)*8)
+    return 1 << (col + (row - 1) * 8)
 
 
-def alg_to_shift(s:str) -> int:
-    columns = ['a','b','c','d','e','f', 'g','h']
+def alg_to_shift(s: str) -> int:
+    columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
     col = columns.index(s[0])
     row = int(s[1])
-    return (col + (row-1)*8)
+    return col + (row - 1) * 8
+
 
 def generate_pawn_move(start: str, side: Colour) -> int:
     loc = alg_to_int(start)
@@ -111,13 +108,14 @@ def generate_pawn_move(start: str, side: Colour) -> int:
         raise Exception(f"you fucked up, {start}")
     return move
 
+
 def generate_pawn_attack(start: str, side: Colour) -> int:
     moves = []
     if side == Colour.white:
         moves = [(1, 1), (-1, 1)]
     else:
         moves = [(1, -1), (-1, -1)]
-    
+
     sPos = Position(start)
 
     int_attack = 0
@@ -125,7 +123,7 @@ def generate_pawn_attack(start: str, side: Colour) -> int:
         new_attack = sPos.add(i[0], i[1])
         if new_attack is not None:
             int_attack |= new_attack.getInt()
-        
+
     return int_attack
 
 
@@ -136,11 +134,12 @@ def generate_knight_move(start: str) -> int:
     for i in perms:
         new_attack = sPos.add(i[0], i[1])
         if new_attack is not None:
-            #print(new_attack.getString())
-            #print(display_binary(new_attack.getInt()))
+            # print(new_attack.getString())
+            # print(display_binary(new_attack.getInt()))
             int_attack |= new_attack.getInt()
-        
+
     return int_attack
+
 
 def generate_king_move(start: str) -> int:
     perms = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
@@ -149,62 +148,64 @@ def generate_king_move(start: str) -> int:
     for i in perms:
         new_attack = sPos.add(i[0], i[1])
         if new_attack is not None:
-            #print(new_attack.getString())
-            #print(display_binary(new_attack.getInt()))
+            # print(new_attack.getString())
+            # print(display_binary(new_attack.getInt()))
             int_attack |= new_attack.getInt()
-        
+
     return int_attack
 
+
 def all_pawn_moves(side: Colour):
-    data = {"start" : [], "move": []}
-    for p in PositionIter('a2', 'h7'):
+    data = {"start": [], "move": []}
+    for p in PositionIter("a2", "h7"):
         m = generate_pawn_move(p, side)
-        data['start'].append(alg_to_int(p))
-        data['move'].append(m)
+        data["start"].append(alg_to_int(p))
+        data["move"].append(m)
     return data
 
 
 def all_pawn_attacks(side: Colour):
-    data = {"start" : [], "move": []}
-    for p in PositionIter('a2', 'h7'):
+    data = {"start": [], "move": []}
+    for p in PositionIter("a2", "h7"):
         m = generate_pawn_attack(p, side)
-        data['start'].append(alg_to_int(p))
-        data['move'].append(m)
+        data["start"].append(alg_to_int(p))
+        data["move"].append(m)
     return data
 
 
 def all_knight_moves():
-    data = {"start" : [], "move": []}
-    for p in PositionIter('a1', 'h8'):
+    data = {"start": [], "move": []}
+    for p in PositionIter("a1", "h8"):
         m = generate_knight_move(p)
-        data['start'].append(alg_to_int(p))
-        data['move'].append(m)
+        data["start"].append(alg_to_int(p))
+        data["move"].append(m)
     return data
 
+
 def all_king_moves():
-    data = {"start" : [], "move": []}
-    for p in PositionIter('a1', 'h8'):
+    data = {"start": [], "move": []}
+    for p in PositionIter("a1", "h8"):
         m = generate_king_move(p)
-        data['start'].append(alg_to_int(p))
-        data['move'].append(m)
+        data["start"].append(alg_to_int(p))
+        data["move"].append(m)
     return data
 
 
 def print_move(start: int, move: int) -> None:
     move_str = display_binary(move)
     start_str = display_binary(start)
-    idx = start_str.index('1')
-    move_str = move_str[:idx] + 'S' + move_str[idx + 1:]
+    idx = start_str.index("1")
+    move_str = move_str[:idx] + "S" + move_str[idx + 1 :]
     print(move_str)
 
 
-'''
+"""
 for i in sys.argv[1:]:
     print(i)
     print(display_binary(int(i)))
-'''
+"""
 
-'''
+"""
 knight_attacks = ['b1', 'd1', 'a2', 'e2', 'a4', 'e4', 'b5', 'd5']
 knight_attacks_masks = [alg_to_int(i) for i in knight_attacks]
 k = 0
@@ -234,18 +235,16 @@ for idx, row in df.iterrows():
     print(' ')
 
 #df.to_csv('test_data/black_pawn_move.csv')
-'''
+"""
 
 
 df = pd.DataFrame(all_knight_moves())
-print(' ')
+print(" ")
 for idx, row in df.iterrows():
     print(row)
-    s = int(row['start'])
-    m = int(row['move'])
+    s = int(row["start"])
+    m = int(row["move"])
     print_move(s, m)
-    print(' ')
+    print(" ")
 
-df.to_csv('data/knight_attacks.csv')
-
-
+df.to_csv("data/knight_attacks.csv")
